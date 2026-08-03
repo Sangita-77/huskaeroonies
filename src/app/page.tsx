b2047eedd14a7d906fc460b494a5ec6ef9bf7a9f
developer <developer@dreamlogodesign.net>
2026-08-03 16:19:12 +0530

    on index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import styles from "./globals.css";
-// import Index from "./index";
+import Index from "./index";
 import SamplePage from "./pages/sample-read";
 export default function Home() {
   return (
     <div>
-        {/* <Index/> */}
+        <Index/>
         <SamplePage/>
     </div>
   );
