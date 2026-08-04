b11a21046c69f7c3ffd8de5479364fab9382531f
Sangita-77 <sangita@dreamlogodesign.com>
2026-08-04 19:09:35 +0530

    all Position

--- src/app/Components/header.tsx
+++ src/app/Components/header.tsx
@@ -1,12 +1,13 @@
 import Navbar from "./Navbar";
-// import Logo from "./Logo";
+import Image from "next/image";
+import Logo from "../Images/logo.svg";
 import styles from "../page.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headercontainer}>
-        {/* <Logo /> */}
+        <a href="/"><Image src={Logo} alt="Bridge" className={styles.logo}/></a>
         <Navbar />
       </div>
     </header>
